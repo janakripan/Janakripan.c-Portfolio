84e9900c07999f2bd0990029863f5811f487e8e3
Anujith vk <anolianu123@gmail.com>
2025-05-20 19:49:54 +0530

    'seo-crash-solved'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Anujith VK | Software Engineer | Thalassery | Kerala | India",
@@ -50,7 +40,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
