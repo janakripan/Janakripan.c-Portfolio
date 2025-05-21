c03a148a2750614da1ebd3c38f2c77a6264a9858
Anujith vk <anolianu123@gmail.com>
2025-05-21 22:10:34 +0530

    'image-changed'

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -56,7 +56,7 @@ keywords: [
     siteName: "Anujith VK Portfolio",
     images: [
       {
-        url: "https://anujith-vk-portfolio.vercel.app/Assets/black2.jpg",
+        url: "https://anujith-vk-portfolio.vercel.app/Assets/black2.png",
         width: 1200,
         height: 630,
         alt: "Anujith VK Portfolio",
